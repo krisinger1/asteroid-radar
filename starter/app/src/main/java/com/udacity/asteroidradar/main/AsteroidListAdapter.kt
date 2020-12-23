09f4a009725c77e5a7b31e185137701f8dca38de
Kristin Risinger <krisinger1@gmail.com>
2020-12-22 19:41:32 -0600

    added test items to recyclerView and created list adapter, added navigation to detail screen

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/AsteroidListAdapter.kt
@@ -0,0 +1,47 @@
+package com.udacity.asteroidradar.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.databinding.ListItemAsteroidBinding
+
+class AsteroidListAdapter(val clickListener : AsteroidListener) : ListAdapter<Asteroid, AsteroidListAdapter.ViewHolder>(DiffCallback()) {
+    class ViewHolder(private var binding: ListItemAsteroidBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(asteroid: Asteroid){
+            binding.asteroid=asteroid
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ListItemAsteroidBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.itemView.setOnClickListener {
+            clickListener.onClick(item)
+            }
+        holder.bind(item)
+    }
+
+    class AsteroidListener(val onClickListener : (asteroid:Asteroid) -> Unit) {
+        fun onClick(asteroid: Asteroid) =onClickListener(asteroid)
+    }
+}
+
+
+
+class DiffCallback : DiffUtil.ItemCallback<Asteroid>() {
+    override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+        return oldItem.id==newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+        return oldItem==newItem
+    }
+
+}
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,33 @@
 package com.udacity.asteroidradar.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.asteroidradar.Asteroid
 
 class MainViewModel : ViewModel() {
+
+    private val _navigateToAsteroidDetails = MutableLiveData<Asteroid>()
+    val navigateToAsteroidDetails : LiveData<Asteroid>
+        get()=_navigateToAsteroidDetails
+
+    private val _asteroidList = MutableLiveData<List<Asteroid>>()
+    val asteroidList : LiveData<List<Asteroid>>
+        get()=_asteroidList
+
+
+    init{
+        val asteroid1= Asteroid(1,"AB123","2020-2-2",1.0,.3,5.0, 100.0, true)
+        val asteroid2= Asteroid(1,"CD345","2020-2-2",2.0,.3,4.0, 50.0, false)
+
+        _asteroidList.value=listOf(asteroid1, asteroid2)
+    }
+
+    fun displayAsteroidDetails(asteroid: Asteroid){
+        _navigateToAsteroidDetails.value=asteroid
+    }
+
+    fun displayAsteroidDetailsComplete(){
+        _navigateToAsteroidDetails.value=null
+    }
 }
